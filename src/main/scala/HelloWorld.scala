231792dc21a248000d269f9412033d4edba59543
David Abrahams <davidabrahams@sbcglobal.net>
2017-05-01 22:47:36 -0400

    built in pagerank works on sample dataset

--- src/main/scala/HelloWorld.scala
+++
@@ -1,7 +0,0 @@
-/**
-  * Created by david on 4/26/17.
-  */
-object HelloWorld {
-  def main(args: Array[String]) =
-    println("Poopie Face")
-}
---
+++ src/main/scala/PageRank.scala
@@ -0,0 +1,34 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.graphx.{Edge, Graph, VertexId}
+import org.apache.spark.rdd.RDD
+
+object PageRank {
+
+  case class WebPage(id: String, url: String)
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf().setMaster("local").setAppName("POOP")
+    val sc = new SparkContext(conf)
+
+    val vertex_data: RDD[String] = sc.textFile("data/shorter_vertices.txt")
+    val pages: RDD[WebPage] = vertex_data.map(_.split(' ')).
+      map(line => WebPage(line(0).trim, line(1).trim))
+    val vertices: RDD[(VertexId, String)] = pages.map(a => (a.id.toLong, a.url))
+
+    vertices.take(10).foreach(println)
+
+    val edge_data: RDD[String] = sc.textFile("data/shorter_edges.txt")
+    val edges: RDD[Edge[Double]] = edge_data.map(_.split(' ')).map(l => Edge(l(0).toLong, l(1).toLong, 1.0))
+
+    val graph = Graph(vertices, edges, "")
+
+    val prBerkeley = graph.staticPageRank(5).cache
+    graph.outerJoinVertices(prBerkeley.vertices) {
+      (v, title, r) => (r.getOrElse(0.0), title)
+    }.vertices.top(10) {
+      Ordering.by((entry: (VertexId, (Double, String))) => entry._2._1)
+    }.foreach(t => println(t._2._2 + ": " + t._2._1))
+
+  }
+}
