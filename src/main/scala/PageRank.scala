72a053148c7ef9410b029023a93a9ed231b10747
David Abrahams <davidabrahams@sbcglobal.net>
2017-05-04 20:05:32 -0400

    removing page object

--- src/main/scala/PageRank.scala
+++ src/main/scala/PageRank.scala
@@ -5,11 +5,13 @@ import org.apache.spark.rdd.RDD
 
 object PageRank {
 
-  case class WebPage(id: String, url: String, pageRank: Double)
-
-  def runPageRank(graph: Graph[WebPage, Double]) = {
+  def runPageRank(graph: Graph[String, Double]) = {
     val oldPageRanks : RDD[(Long, Double)] = graph.vertices.map(v => (v._1, 1.0 / graph.numVertices))
     val newPageRanks : RDD[(Long, Double)] = graph.vertices.map(v => (v._1, 1.0 / graph.numVertices))
+    var ranks = graph.vertices.mapValues((v : String) => v)
+    for (i <- 1 to 10) {
+      val s = oldPageRanks.map((t: (Long,Double)) => (t._1, 0.15/graph.numVertices))
+    }
 
     // TODO
     // Run loop and update page ranks!
@@ -28,16 +30,15 @@ object PageRank {
 
 //    val vertex_data: RDD[String] = sc.textFile("data/shorter_vertices.txt")
     val vertex_data: RDD[String] = sc.textFile("s3n://dcomp-pagerank/shorter_vertices.txt")
-    val pages: RDD[WebPage] = vertex_data.map(_.split(' ')).
-      map(line => WebPage(line(0).trim, line(1).trim, 0.0))
-    val vertices: RDD[(VertexId, WebPage)] = pages.map(a => (a.id.toLong, a))
+    val vertices: RDD[(VertexId, String)] = vertex_data.map(_.split(' ')).
+      map(line => (line(0).trim.toLong, line(1).trim))
 
     vertices.take(10).foreach(println)
 
     val edge_data: RDD[String] = sc.textFile("s3n://dcomp-pagerank/shorter_edges.txt")
     val edges: RDD[Edge[Double]] = edge_data.map(_.split(' ')).map(l => Edge(l(0).toLong, l(1).toLong, 1.0))
 
-    val graph: Graph[WebPage, Double] = Graph(vertices, edges, null)
+    val graph: Graph[String, Double] = Graph(vertices, edges, null)
 
 //    val prGraph = graph.staticPageRank(5).cache
 //    graph.outerJoinVertices(prGraph.vertices) {
