55b9e96742874dea4935873a02862e31d7baaa4b
David Abrahams <davidabrahams@sbcglobal.net>
2017-05-04 21:47:47 -0400

    it compiles

--- src/main/scala/PageRank.scala
+++ src/main/scala/PageRank.scala
@@ -1,17 +1,42 @@
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.graphx.{Edge, Graph, VertexId}
+import org.apache.spark.graphx._
 import org.apache.spark.rdd.RDD
 
 
 object PageRank {
 
   def runPageRank(graph: Graph[String, Double]) = {
-    val oldPageRanks : RDD[(Long, Double)] = graph.vertices.map(v => (v._1, 1.0 / graph.numVertices))
-    val newPageRanks : RDD[(Long, Double)] = graph.vertices.map(v => (v._1, 1.0 / graph.numVertices))
-    var ranks = graph.vertices.mapValues((v : String) => v)
+    // Make a graph whose vertex property is its out degree
+    val outDegreeGraph : Graph[Int, Double] = graph.outerJoinVertices(graph.outDegrees) { (vid, vdata, deg) => deg.getOrElse(0) }
+    // Construct a graph where each edge contains the weight
+    // and each vertex is the initial PageRank
+
+    // create a graph whose vertices contain initial page rank and edges contain initial weights (1/outDegree)
+    val outputGraph: Graph[Double, Double] = outDegreeGraph.
+      // store edge weights
+      mapTriplets(edgeTriplet => 1.0 / edgeTriplet.srcAttr).
+      // store initial page rank
+      mapVertices((id, _) => 1.0 / graph.numVertices)
+
     for (i <- 1 to 10) {
-      val s = oldPageRanks.map((t: (Long,Double)) => (t._1, 0.15/graph.numVertices))
+      val rankUpdates : RDD[(Long, Double)] = outputGraph.aggregateMessages[Double] (
+        // Send message to each triplet in graph containing pageRank * edgeWeight
+        edgeTriplet => edgeTriplet.sendToDst(edgeTriplet.srcAttr * edgeTriplet.attr),
+        // Merge message. Accumulate page ranks
+        _ + _,
+        // Which triplet fields to include
+        TripletFields.Src)
+
+
     }
+
+
+
+
+
+
+
+
 
     // TODO
     // Run loop and update page ranks!
@@ -30,13 +55,17 @@ object PageRank {
 
 //    val vertex_data: RDD[String] = sc.textFile("data/shorter_vertices.txt")
     val vertex_data: RDD[String] = sc.textFile("s3n://dcomp-pagerank/shorter_vertices.txt")
-    val vertices: RDD[(VertexId, String)] = vertex_data.map(_.split(' ')).
+    val vertices: RDD[(Long, String)] = vertex_data.map(_.split(' ')).
       map(line => (line(0).trim.toLong, line(1).trim))
 
     vertices.take(10).foreach(println)
 
     val edge_data: RDD[String] = sc.textFile("s3n://dcomp-pagerank/shorter_edges.txt")
-    val edges: RDD[Edge[Double]] = edge_data.map(_.split(' ')).map(l => Edge(l(0).toLong, l(1).toLong, 1.0))
+    val edges: RDD[Edge[Double]] = edge_data
+      // Split on spaces
+      .map(_.split(' '))
+      // Create edge objects
+      .map(l => Edge(l(0).toLong, l(1).toLong, 1.0))
 
     val graph: Graph[String, Double] = Graph(vertices, edges, null)
 
